25b9fc57d528bd0e703755e3d57f0708a4d25184
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-21 16:52:52 +0300

    Modify the class method generate credentials to a static method

--- credential.py
+++ credential.py
@@ -23,12 +23,21 @@ class Credential:
         '''
         Credential.credential_list.append(self)
 
-    def generate_credentials(self):
+    @staticmethod
+    def generate_credentials():
         '''
         A method to auto-generate user credentials
         '''
         auth_string = string.ascii_uppercase + string.digits + string.ascii_lowercase
         auth_credential = ''.join(choice(auth_string) for item in range(10))
         return auth_credential
+
+    # @classmethod
+    # def display_credentials(cls, login):
+    #     '''
+    #     Display a list of saved credentials
+    #     '''
+    #     credentials = [credential for credential in cls.credential_list if credential.user == login]
+    #     return credentials
 
     
