af1e23890a54fb40ce1c08a54379166ba736c467
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-21 19:58:37 +0300

    set up and tear down methods to instantiate the user and credential classes and reset class variables

--- test.py
+++ test.py
@@ -7,5 +7,19 @@ class TestPasswordLocker(unittest.TestCase):
     Test class for the all password management functionality
     '''
 
-   
+    def setUp(self):
+        '''
+        set up method to set up a user instance and a credential instance
+        '''
+        self.user1 = User('Jdoe', 'password')
+        self.first_credential = Credential(
+            'Jdoe', 'password', 'instagram', 'ig_password')
+
+    def tearDown(self):
+        '''
+        Tear down method to reset the class variables after each test
+        '''
+
+        User.user_list = []
+        Credential.credential_list = []
 
