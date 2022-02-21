a74922da04ffc78ea671ed97f6c43693a71785c2
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-21 14:25:01 +0300

    Add a class variable to hold a list of all created credentials

--- credential.py
+++ credential.py
@@ -3,6 +3,9 @@ class Credential:
     Credentials class to describe saved credantials
     '''
 
+    # A list of created/store credentials
+    credential_list = []
+
     def __init__(self, user, platform_name, platform_password) -> None:
         '''
         credential class initialiser method
@@ -10,3 +13,5 @@ class Credential:
         self.user = user
         self.platform_name = platform_name
         self.platform_password = platform_password
+
+    
