ebcb3a8d1a5093e2fc5a1b677468b7608df05f94
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-21 19:56:49 +0300

    Add a test class that inherits from TestCase from the unittest module

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+import unittest
+from credential import Credential
+from user import User
 
+class TestPasswordLocker(unittest.TestCase):
+    '''
+    Test class for the all password management functionality
+    '''
+
+   
 
